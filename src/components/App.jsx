2aca0c8810844114b664b538f2e7b6d0f9f46e0d
TauruS111 <yuriybereza85@gmail.com>
2023-12-13 22:39:10 +0100

    add/fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,75 +1,3 @@
-// import { Component } from 'react';
-// import { nanoid } from 'nanoid';
-
-// import { ContactForm } from './ContactForm/ContactForm';
-// import { ContactList } from './ContactList/ContactList';
-// import { Filter } from './Filter/Filter';
-
-// export class App extends Component {
-//   state = {
-//     contacts: [],
-//     filter: '',
-//   };
-
-//   handleAddContact = ({ name, number }) => {
-//     if (
-//       this.state.contacts.some(
-//         contact => contact.name.toLowerCase() === name.toLowerCase()
-//       )
-//     ) {
-//       alert(`${name} is already in contacts.`);
-//       return;
-//     }
-
-//     const newContact = {
-//       id: nanoid(),
-//       name,
-//       number,
-//     };
-
-//     this.setState(prevState => ({
-//       contacts: [...prevState.contacts, newContact],
-//     }));
-//   };
-
-//   handleDeleteContact = id => {
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(contact => contact.id !== id),
-//     }));
-//   };
-
-//   handleFilterChange = filter => {
-//     this.setState({ filter });
-//   };
-
-//   getFilteredContacts = () => {
-//     const { contacts, filter } = this.state;
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(filter.toLowerCase())
-//     );
-//   };
-
-//   render() {
-//     const filteredContacts = this.getFilteredContacts();
-
-//     return (
-//       <div className="container">
-//         <h1>Phonebook</h1>
-//         <div className="contact">
-//           <ContactForm onSubmit={this.handleAddContact} />
-//         </div>
-//         <h2>Contacts</h2>
-//         <h4>Find contacts by name</h4>
-//         <Filter filter={this.state.filter} onChange={this.handleFilterChange} />
-//         <ContactList
-//           contacts={filteredContacts}
-//           onDelete={this.handleDeleteContact}
-//         />
-//       </div>
-//     );
-//   }
-// }
-
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 
@@ -84,7 +12,6 @@ export class App extends Component {
   };
 
   componentDidMount() {
-    // Зчитуємо дані з localStorage під час завантаження компонента
     const savedContacts = localStorage.getItem('contacts');
 
     if (savedContacts) {
@@ -93,7 +20,6 @@ export class App extends Component {
   }
 
   componentDidUpdate(_, prevState) {
-    // Зберігаємо контакти у localStorage після кожного оновлення стану
     if (prevState.contacts !== this.state.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
